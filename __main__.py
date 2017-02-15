fca64d8c7790ff16f855c325570bb1d1daa9a620
jsletellier <js.letellier@gmail.com>
2017-02-15 12:06:15 -0500

    Extracted the api to it's own object and starting it from main

---
+++ __main__.py
@@ -0,0 +1,18 @@
+import json
+
+from api.restapi import FlaskRESTAPI
+
+from camera.camera import Camera
+from camera.camera import CameraModel
+
+if __name__ == "__main__":
+    with open('./config/camera_model.json') as file:
+        camera_parameters = json.load(file)
+
+        camera = Camera(
+            CameraModel(None, None, camera_parameters['camera_matrix'], None, None, None, None)
+        )
+
+    api = FlaskRESTAPI(static_folder='../static', camera_service=camera)
+
+    api.run()
