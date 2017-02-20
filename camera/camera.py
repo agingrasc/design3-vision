6f3dd9e5fc54fc9eadbf03b38f97af30a23f6a7f
jsletellier <js.letellier@gmail.com>
2017-02-20 09:42:47 -0500

    Refactor, extracted method to make explicit extrinsic parameters creation

--- camera/camera.py
+++ camera/camera.py
@@ -63,7 +63,8 @@ class CameraFactory:
     def create_camera_model(self, intrinsic_parameters, translation_vector, rotation_vector,
                             distortion_coefficients, origin):
         rotation_matrix = self._get_rotation_matrix_from(rotation_vector)
-        extrinsic_parameters = np.concatenate((rotation_matrix, translation_vector), axis=1)
+        extrinsic_parameters = self._get_extrinsic_parameters(rotation_matrix, translation_vector)
+
         camera_matrix = self._compute_camera_matrix(intrinsic_parameters, extrinsic_parameters)
 
         return CameraModel(
@@ -78,6 +79,9 @@ class CameraFactory:
     def _get_rotation_matrix_from(self, rotation_vector):
         rotation_matrix, jacobian = cv2.Rodrigues(rotation_vector)
         return rotation_matrix
+
+    def _get_extrinsic_parameters(self, rotation_matrix, translation_vector):
+        return np.concatenate((rotation_matrix, translation_vector), axis=1)
 
     def _compute_camera_matrix(self, intrinsic_parameters, extrinsic_parameters):
         return np.dot(intrinsic_parameters, extrinsic_parameters)
