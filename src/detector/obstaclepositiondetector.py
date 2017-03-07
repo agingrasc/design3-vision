c6e6d4a22a0bc56fdf256c102579225d9e03bc24
jeanemmanuelstamour <jeanemmanuelstamour1@gmail.com>
2017-03-07 11:21:56 -0500

    import circledetector class

--- src/detector/obstaclepositiondetector.py
+++ src/detector/obstaclepositiondetector.py
@@ -2,6 +2,7 @@ import cv2
 import glob
 import math
 import numpy as np
+from .robotpositiondetector import CircleDetector
 
 if __name__ == '__main__':
     img = cv2.imread('../../data/images/robot_images/image4.jpg', 0)
@@ -9,7 +10,8 @@ if __name__ == '__main__':
     cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
     circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20,
-                            param1=50, param2=60, minRadius=20, maxRadius=40)
+                             param1=50, param2=60, minRadius=20, maxRadius=40)
+    circles = CircleDetector(20, 20, 40)
 
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
