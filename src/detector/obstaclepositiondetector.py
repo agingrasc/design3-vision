c3f989abc12a94f134d3a2b996919e63ad1340fa
jeanemmanuelstamour <jeanemmanuelstamour1@gmail.com>
2017-03-08 21:16:34 -0500

    some changes in robotpositiondetector and obstaclepositiondetector

--- src/detector/obstaclepositiondetector.py
+++ src/detector/obstaclepositiondetector.py
@@ -1,25 +1,56 @@
 import cv2
 import glob
-import math
-import numpy as np
-from .robotpositiondetector import CircleDetector
+
+from infrastructure.camera import JSONCameraModelRepository
+from detector.robotpositiondetector import CircleDetector
+from detector.robotpositiondetector import NoMatchingCirclesFound
+
+
+RATIO = 1
+TARGET_MIN_DISTANCE = 20
+TARGET_MIN_RADIUS = 20
+TARGET_MAX_RADIUS = 40
+
+
+class ObstacleDetector:
+    def __init__(self):
+        pass
+
+    def detect_obstacle(self, img):
+        img = self._median_blur(img)
+        cimg = self._convert_color_image(img)
+        obstacles = CircleDetector(RATIO, TARGET_MIN_DISTANCE, TARGET_MIN_RADIUS,
+                TARGET_MAX_RADIUS).detect_obstacles_markers(img)
+        for i in obstacles[0, :]:
+            # draw the outer circle
+            cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            # draw the center of the circle
+            cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+        return cimg
+
+    def _median_blur(self, img):
+        img = cv2.medianBlur(img, 5)
+        return img
+    def _convert_color_image(self, img):
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        return img
 
 if __name__ == '__main__':
-    img = cv2.imread('../../data/images/robot_images/image4.jpg', 0)
-    img = cv2.medianBlur(img, 5)
-    cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    obstacle_detector = ObstacleDetector()
+    camera_repository = JSONCameraModelRepository('../../data/camera_models/camera_models.json')
+    camera_model = camera_repository.get_camera_model_by_id(0)
 
-    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20,
-                             param1=50, param2=60, minRadius=20, maxRadius=40)
-    circles = CircleDetector(20, 20, 40)
+    images = glob.glob('../../data/images/full_scene/*.jpg')
 
-    circles = np.uint16(np.around(circles))
-    for i in circles[0, :]:
-         # draw the outer circle
-         cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-         # draw the center of the circle
-         cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+    for filename in images:
+        image = cv2.imread(filename, 0)
 
-    cv2.imshow('detected circles', cimg)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        image = camera_model.undistort_image(image)
+
+        try:
+            cimg = obstacle_detector.detect_obstacle(image)
+        except NoMatchingCirclesFound:
+            pass
+
+        cv2.imshow('detected circles', cimg)
+        cv2.waitKey(2000)
--- src/detector/robotpositiondetector.py
+++ src/detector/robotpositiondetector.py
@@ -6,6 +6,7 @@ import numpy as np
 from infrastructure.camera import JSONCameraModelRepository
 
 NUMBER_OF_MARKERS = 3
+RATIO = 1.7
 TARGET_MIN_DISTANCE = 12
 TARGET_MIN_RADIUS = 5
 TARGET_MAX_RADIUS = 30
@@ -20,13 +21,14 @@ class NoMatchingCirclesFound(Exception):
     pass
 
 class CircleDetector:
-    def __init__(self, min_distance, min_radius, max_radius):
+    def __init__(self, ratio, min_distance, min_radius, max_radius):
+        self.ratio = ratio
         self._min_distance = min_distance
         self._min_radius = min_radius
         self._max_radius = max_radius
 
-    def detect(self, image):
-        circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1.7, self._min_distance, param1=50, param2=30,
+    def detect_robot_markers(self, image):
+        circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, self.ratio, self._min_distance, param1=50, param2=30,
                                          minRadius=self._min_radius,
                                          maxRadius=self._max_radius)
 
@@ -37,13 +39,24 @@ class CircleDetector:
         else:
             raise NoMatchingCirclesFound
 
+    def detect_obstacles_markers(self, image):
+        circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, self.ratio, self._min_distance, param1=50, param2=60,
+                                         minRadius=self._min_radius,
+                                         maxRadius=self._max_radius)
+
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            return circles
+        else:
+            raise NoMatchingCirclesFound
+
 
 class RobotPositionDetector:
     def detect_position(self, image):
         image = self._preprocess(image)
         threshold = self._threshold_robot_makers(image)
 
-        robot_markers = CircleDetector(TARGET_MIN_DISTANCE, TARGET_MIN_RADIUS, TARGET_MAX_RADIUS).detect(threshold)
+        robot_markers = CircleDetector(RATIO, TARGET_MIN_DISTANCE, TARGET_MIN_RADIUS, TARGET_MAX_RADIUS).detect_robot_markers(threshold)
         robot_position = self._get_robot_position(robot_markers)
 
         if self._missing_markers(robot_markers):
@@ -171,7 +184,7 @@ if __name__ == '__main__':
             cv2.arrowedLine(image, (0, 0), (50, 0), (0, 255, 0), 3)
             cv2.putText(image, str(degrees), tuple(robot_position['direction'][1]), fontFace=cv2.FONT_HERSHEY_PLAIN,
                         fontScale=1.0, color=(255, 255, 255))
-        except NoRobotMarkersFound:
+        except NoMatchingCirclesFound:
             pass
 
         cv2.imshow("Position", image)
