1fc6d175b47eda5413bef2a801d1b97f22715896
jsletellier <js.letellier@gmail.com>
2017-03-17 21:08:51 -0400

    [WIP] added multithreading to boost performance

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ BASESTATION_WEBSOCKET_URL = 'ws://localhost:3000'
 CAMERA_MODELS_FILE_PATH = '../data/camera_models/camera_models.json'
 TEST_IMAGE_DIRECTORY_PATH = '../data/images/full_scene/*.jpg'
 TABLE_CAMERA_MODEL_ID = 0
-CAMERA_ID = 1
+CAMERA_ID = 0
 CAP_WIDTH = 1280
 CAP_HEIGHT = 800
 
