7b9c41949260b77f6ec6c00a27f53aa54d9e7558
jsletellier <js.letellier@gmail.com>
2017-04-11 10:01:53 -0400

    Added robot feed to websocket

--- scripts/pull_robot_feed.py
+++ scripts/pull_robot_feed.py
@@ -1,15 +1,25 @@
 import datetime
+import json
+from time import sleep
 
 import cv2
 
+from websocket import create_connection
+
+from infrastructure.imagesource.savevideoimagesource import SaveVideoImageSource
 from src.infrastructure.imagesource.httpimagesource import HTTPImageSource
 from src.service.image.imagesegmentation import segment_image
+from src.infrastructure.messageassembler import MessageAssembler
 
 ROBOT_VIDEO_SERVICE_URL = "http://192.168.0.27:4040/take-picture"
 
 if __name__ == '__main__':
+    connection = create_connection("ws://localhost:3000")
+    message_assembler = MessageAssembler()
+
     index = 0
     image_source = HTTPImageSource(ROBOT_VIDEO_SERVICE_URL)
+    # image_source = SaveVideoImageSource('/Users/jeansebastien/Desktop/videos/robot_video2.avi')
 
     while True:
         print('Fetching image: {}'.format(datetime.datetime.now()))
@@ -18,14 +28,18 @@ if __name__ == '__main__':
 
         try:
             found_segments, inner_figure, center_of_mass, figure_mask = segment_image(image)
+            figure_mask = cv2.resize(figure_mask, None, fx=0.7, fy=0.7, interpolation=cv2.INTER_CUBIC)
             cv2.imshow('thres', inner_figure)
-
+            cv2.imshow('figure mask', figure_mask)
         except Exception as e:
-            print("NoSegmentsInImage")
+            print(type(e).__name__)
 
         image = cv2.resize(image, None, fx=0.7, fy=0.7, interpolation=cv2.INTER_CUBIC)
 
-        cv2.imshow('image', image)
+        cv2.imshow('Image', image)
+
+        # connection.send(json.dumps({"headers": "push_robot_feed", "data": {"image": message_assembler.prepare_image(image) }}))
+
         # cv2.imwrite('../data/images/robot_feed/image{}.jpg'.format(index), image)
-        index += 1
+        # index += 1
         cv2.waitKey(1)
